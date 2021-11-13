37e8ca1bd2ff08c380fb7f591b9701f9680f2a85
Florian Delrieu <florian.delrieu@free.fr>
2021-11-13 13:15:47 +0100

    change constant speed move

--- Codes/Player.gd
+++ Codes/Player.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
 var vel = Vector2()
+const speed = 100
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	vel.x = 0
+	vel.y = 0
+	
 	if Input.is_action_pressed("ui_right"):  # detecte le mapping de touche "ui_right"
-		vel.x += 1
+		vel.x += speed
 	if Input.is_action_pressed("ui_left"):
-		vel.x -= 1
+		vel.x -= speed
 	if Input.is_action_pressed("ui_down"):
-		vel.y += 1
+		vel.y += speed
 	if Input.is_action_pressed("ui_up"):
-		vel.y -= 1
+		vel.y -= speed
 	move_and_slide(vel) # bouge le slide de la vitesse :vel:
 	
 	print("Speed :", vel.x," , ", vel.y)
