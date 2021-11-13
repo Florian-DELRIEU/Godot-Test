e549a0b115e790ba43bf1b15e4b6a6ee29af8bdd
Florian Delrieu <florian.delrieu@free.fr>
2021-11-13 13:52:29 +0100

    add Gravity and cumulative direction

--- Codes/Player.gd
+++ Codes/Player.gd
@@ -2,20 +2,27 @@ extends KinematicBody2D
 
 var vel = Vector2()
 var speed
+const GRAVITY = 9.81 + 10
 
 func _process(delta):
+	pass
+	
+func _physics_process(delta):
 	vel.x = 0
-	vel.y = 0
-	speed = 200 if Input.is_action_pressed("Shift") else 100
+	vel.y += GRAVITY * delta
+	mouvement_loop(delta)
+		
+func mouvement_loop(delta):
+	var speed = 200 if Input.is_action_pressed("Shift") else 100
+	var right = Input.is_action_pressed("ui_right") # detecte le mapping de touche "ui_right"
+	var left  = Input.is_action_pressed("ui_left")
+	var down  = Input.is_action_pressed("ui_down")
+	var up    = Input.is_action_pressed("ui_up")
 	
-	if Input.is_action_pressed("ui_right"):  # detecte le mapping de touche "ui_right"
-		vel.x += speed
-	if Input.is_action_pressed("ui_left"):
-		vel.x -= speed
-	if Input.is_action_pressed("ui_down"):
-		vel.y += speed
-	if Input.is_action_pressed("ui_up"):
-		vel.y -= speed
+	var dirX = int(right) - int(left)
+	var dirY = int(down) - int(up) + GRAVITY * delta
+	print(dirX, " , ", dirY)
+	
+	vel.x = speed*dirX
+	vel.y = speed*dirY
 	move_and_slide(vel) # bouge le slide de la vitesse :vel:
-	
-	print("Speed :", vel.x," , ", vel.y)
