0287cefc263d021a35b930bef6f6b95a72eda246
floriandelrieu <florian.delrieu@free.fr>
2021-11-14 15:35:43 +0100

    exports variables

--- Codes/Player.gd
+++ Codes/Player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 # Constants
-const GRAVITY = 1000
-const JUMP = 700
 const UP_direction = Vector2(0,-1)
-const ACC = 30
+export (int) var GRAVITY = 1000
+export (int) var ACC = 30
+export (int) var JUMP = 700
+export (int) var max_speed = 200
 # Variables
 var vel = Vector2()
-var max_speed
 
 func _physics_process(delta):
 	vel.y += GRAVITY * delta
@@ -18,13 +18,13 @@ func mouvement_loop(delta):
 	var left  = Input.is_action_pressed("ui_left")
 	var jump  = Input.is_action_just_pressed("ui_select")
 	
-	var max_speed = 400 if Input.is_action_pressed("Shift") else 200
+	var curr_max_speed = 2*max_speed if Input.is_action_pressed("Shift") else max_speed
 	var dirX = int(right) - int(left)
 # Dir X loop
 	if dirX == +1:
-		vel.x = min(vel.x+ACC,max_speed)
+		vel.x = min(vel.x+ACC,curr_max_speed)
 	elif dirX == -1:
-		vel.x = max(vel.x-ACC,-max_speed)
+		vel.x = max(vel.x-ACC,-curr_max_speed)
 	else:
 		vel.x = lerp(vel.x, 0, 0.80) # Va de vel.x à 0 par tranche de 80%
 # Dir Y loop
