1a76b0a7d434f49e8e90b327ecbedf4e48505a61
floriandelrieu <florian.delrieu@free.fr>
2021-11-14 19:09:24 +0100

    Anim player

--- Codes/Player.gd
+++ Codes/Player.gd
@@ -10,31 +10,37 @@ var vel = Vector2()
 
 func _physics_process(delta):
 	vel.y += GRAVITY * delta
-	mouvement_loop(delta)
+	mouvement_loop(delta) 
 	move_and_slide(vel, UP_direction) # bouge le slide de la vitesse :vel:
 		
 func mouvement_loop(delta):
 	var right = Input.is_action_pressed("ui_right") # detecte le mapping de touche "ui_right"
 	var left  = Input.is_action_pressed("ui_left")
 	var jump  = Input.is_action_just_pressed("ui_select")
-	
-	var curr_max_speed = 2*max_speed if Input.is_action_pressed("Shift") else max_speed
+	var run   = Input.is_action_pressed("Shift")
+	var curr_max_speed = 2*max_speed if run else max_speed
+	var move_anim = "run" if run else "walk"
 	var dirX = int(right) - int(left)
 # Dir X loop
 	if dirX == +1:
 		vel.x = min(vel.x+ACC,curr_max_speed)
 		$Sprite.flip_h = false
+		anim_loop(move_anim)
 	elif dirX == -1:
 		vel.x = max(vel.x-ACC,-curr_max_speed)
 		$Sprite.flip_h = true
+		anim_loop(move_anim)
 	else:
 		vel.x = lerp(vel.x, 0, 0.80) # Va de vel.x à 0 par tranche de 80%
+		anim_loop("idle")
 # Dir Y loop
-	if is_on_floor():
-		if jump: vel.y = -JUMP
-		else: vel.y = 0
+	if jump and is_on_floor():
+		vel.y = -JUMP
+	elif is_on_floor(): vel.y = 0
 # Print on log
 	print(vel.x, " , ", vel.y, " , ", dirX)
 	
-
+func anim_loop(animation):
+	if $Anim.current_animation != animation:
+		$Anim.play(animation)
 	
--- Codes/debug_label.gd
+++ Codes/debug_label.gd
@@ -4,4 +4,3 @@ func _process(delta):
 	var FPS = 1/delta
 	text = ""
 	text += "FPS : %.0f" % FPS
-
