5e27b248db2f6b6466fb38ab2e909a222abb6c85
floriandelrieu <florian.delrieu@free.fr>
2021-11-15 17:49:49 +0100

    add ennemy and animation for it

---
+++ Scenes/Blob.gd
@@ -0,0 +1,59 @@
+extends KinematicBody2D
+# Constants
+const UP_direction = Vector2(0,-1)
+export (int) var GRAVITY = 1000
+export (int) var ACC = 30
+export (int) var JUMP = 700
+export (int) var max_speed = 200
+# Variables
+var vel = Vector2()
+var jump_count = 0
+var curr_max_speed
+var move_anim
+
+func _physics_process(delta):
+	mouvement_loop(delta) 
+	vel.y += GRAVITY * delta
+	vel = move_and_slide(vel, UP_direction) # bouge le slide de la vitesse :vel:
+		
+func mouvement_loop(delta):
+	var right = Input.is_action_pressed("ui_right") # detecte le mapping de touche "ui_right"
+	var left  = Input.is_action_pressed("ui_left")
+	var jump  = Input.is_action_just_pressed("ui_select")
+	var run   = Input.is_action_pressed("Shift")
+	if is_on_floor(): jump_count = 0
+	
+# is running ?
+	if run:
+		curr_max_speed 	= 2*max_speed
+		move_anim 		= "run"
+	else:
+		curr_max_speed 	= max_speed
+		move_anim	 	= "walk"
+		
+	var dirX = int(right) - int(left)
+# Dir X loop
+	if dirX == +1:
+		vel.x = min(vel.x+ACC,curr_max_speed)
+		$Sprite.flip_h = false
+		anim_loop(move_anim)
+	elif dirX == -1:
+		vel.x = max(vel.x-ACC,-curr_max_speed)
+		$Sprite.flip_h = true
+		anim_loop(move_anim)
+	else:
+		vel.x = lerp(vel.x, 0, 0.80) # Va de vel.x à 0 par tranche de 80%
+		anim_loop("idle")
+# Dir Y loop
+	if jump and jump_count <= 1:
+		vel.y = -JUMP
+		jump_count += 1
+	if vel.y > 0: anim_loop("fall")
+	if vel.y < 0: anim_loop("jump")
+# Print on log
+	print(vel.x, " , ", vel.y, " , ", jump_count)
+	
+func anim_loop(animation):
+	if $Anim.current_animation != animation:
+		$Anim.play(animation)
+	
