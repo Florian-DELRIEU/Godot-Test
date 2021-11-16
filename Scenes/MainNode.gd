6a45e6863ad1a6ecbc7234625dc910147120d035
Florian Delrieu <florian.delrieu@free.fr>
2021-11-16 22:01:35 +0100

    add button and connect signals

--- Scenes/MainNode.gd
+++ Scenes/MainNode.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 # onready is like in :func _ready:
-onready var parent = get_node("Parent")
-onready var enfant = get_node("Parent/Enfant")
+onready var parent = get_node("Parent") # = $Parent
+onready var enfant = get_node("Parent/Enfant") # = $Parent/Enfant
+var time = 0
 
 func _process(delta):
+	time += delta
 	parent.position.x += 1
 	enfant.rotation_degrees += 10
+
+
+func ButtonPressed():
+	$Parent/Label.text = "Yeah i'm ready"
