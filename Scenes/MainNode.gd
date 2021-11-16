8e84ce85929bb083af488491752b165e3868fbff
Florian Delrieu <florian.delrieu@free.fr>
2021-11-16 22:11:51 +0100

    Parent restart on click

--- Scenes/MainNode.gd
+++ Scenes/MainNode.gd
@@ -1,15 +1,19 @@
 extends Node2D
-
 # onready is like in :func _ready:
 onready var parent = get_node("Parent") # = $Parent
 onready var enfant = get_node("Parent/Enfant") # = $Parent/Enfant
 var time = 0
+var ci_pos = Vector2(100,100)
+var speed = 2
+var speed_rotate = 10
+
 
 func _process(delta):
 	time += delta
-	parent.position.x += 1
-	enfant.rotation_degrees += 10
+	$Parent.position.x += speed
+	$Parent/Enfant.rotation_degrees += speed_rotate
 
 
 func ButtonPressed():
-	$Parent/Label.text = "Yeah i'm ready"
+	$Parent/Label.text = "Here we go again"
+	$Parent.position = ci_pos
