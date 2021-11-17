1f10912bd921bee7694d4788494e7a9c23c9676f
floriandelrieu <florian.delrieu@free.fr>
2021-11-17 18:47:01 +0100

    Player and debug
    - Player as kinematic
    - Player script
    - add debug label

---
+++ Scenes/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+var left = bool()
+var right = bool()
+var up = bool()
+var down = bool()
+var dirX = int()
+var dirY = int()
+var DEBUG = true
+	
+export (int, 0, 10, 1) var speed
+
+func _process(delta):
+	checkInputs()
+	mooving_loop()
+	printDEBUG(DEBUG)
+
+func checkInputs():
+	left = Input.is_action_pressed("ui_left")
+	right = Input.is_action_pressed("ui_right")
+	up = Input.is_action_pressed("ui_up")
+	down = Input.is_action_pressed("ui_down")
+	dirX = int(right) - int(left)
+	dirY = int(down) - int(up)
+
+func mooving_loop():
+	position.x += dirX * speed
+	position.y += dirY * speed
+
+func printDEBUG(DEBUG_LEVEL):
+	if DEBUG_LEVEL: print( str(dirX) +" , "+ str(dirY) )
--- Scripts/MainNode.gd
+++ Scripts/MainNode.gd
@@ -1,21 +1,19 @@
 extends Node2D
-# onready is like in :func _ready:
-onready var parent = get_node("Parent") # = $Parent
-onready var scene_label = get_node("SceneLabel")
-var time = 0
-export (int) var speed = 2
-var button_pressed = false
+var player_input_x
+var player_input_y
+var txt = ""
 
-func _ready():
-	var ci_x = parent.position.x
-	var ci_y = parent.position.y
-	scene_label.text = "CI : "+str(ci_x)+" , " +str(ci_y)
-	
 func _process(delta):
-	time += delta
-	if button_pressed: $Parent.position.x += speed
-	if not button_pressed: $Parent/ParentLabel.text = str($Parent.position)
+	player_input_x = $Player.dirX
+	player_input_y = $Player.dirY
+	debug_print()
 
-func ButtonPressed():
-	button_pressed = not button_pressed
-	$Parent/ParentLabel.text = "GOOOOO !"
+func debug_print():
+	txt = """DEBUG LABEL
+	Player input: 		{} , {}
+	Player position: 	{} , {}
+	""".format([
+		player_input_x,player_input_y,
+		$Player.position.x,$Player.position.y
+		],"{}")
+	$SceneLabel.text = txt
