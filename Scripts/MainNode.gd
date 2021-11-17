bf657ce183bda65d471a8be9fd2d3a745b3ba6c7
Florian Delrieu <florian.delrieu@free.fr>
2021-11-17 14:09:39 +0100

    move rotation in other script

--- Scripts/MainNode.gd
+++ Scripts/MainNode.gd
@@ -1,11 +1,9 @@
 extends Node2D
 # onready is like in :func _ready:
 onready var parent = get_node("Parent") # = $Parent
-onready var enfant = get_node("Parent/Enfant") # = $Parent/Enfant
 onready var scene_label = get_node("SceneLabel")
 var time = 0
 export (int) var speed = 2
-export (float,0,10,1) var speed_rotate  # selecteur
 var button_pressed = false
 
 var Inventaire = [10,20,30] # Like lists
@@ -18,7 +16,6 @@ func _ready():
 func _process(delta):
 	time += delta
 	$Parent.position.x += speed
-	$Parent/Enfant.rotation_degrees += speed_rotate
 	if not button_pressed: $Parent/ParentLabel.text = str($Parent.position)
 
 
--- Scripts/rotate_sprite.gd
+++ Scripts/rotate_sprite.gd
@@ -1,8 +1,10 @@
 extends Sprite
 
 var angle = 0
-var delta_angle
+var delta_angle = 1
 
 func _process(delta):
 	angle += delta_angle
+	if angle > 359: angle = 0
 	self.rotation_degrees = angle
+	print("Angle = " +str(angle))
