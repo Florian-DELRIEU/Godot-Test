84b5cb5ef4f8c81a8dcff0084fa3c055ed0acb2e
Florian Delrieu <florian.delrieu@free.fr>
2021-11-18 22:14:07 +0100

    bullet scale and PV

--- Scenes/Kineticball.gd
+++ Scenes/Kineticball.gd
@@ -2,11 +2,11 @@ extends RigidBody2D
 
 var initial_direction = rand_range(0,359)
 
-func _ready():
-	linear_velocity.angle_to = initial_direction
 
 func _process(delta):
 	bounce = 1
 	friction = 0
 	linear_damp = 0
+	scale = Vector2(0.5,0.5)
+	$CollisionShape2D.scale = Vector2(0.5,0.5)
 	var current_speed = linear_velocity
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,12 +6,14 @@ var up = bool()
 var down = bool()
 var dirX = int()
 var dirY = int()
+var PV = 100
 
 export (int) var speed = 10
 
 func _process(delta):
 	checkInputs()
 	mooving_loop()
+	$Player_label.text = "PV = " + str(PV)
 
 func checkInputs():
 	left = Input.is_action_pressed("ui_left")
