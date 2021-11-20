66e17ef6793b565656593896cbf8747d52f83144
Florian Delrieu <florian.delrieu@free.fr>
2021-11-20 14:29:57 +0100

    exports coeffs

--- Scripts/Kineticball.gd
+++ Scripts/Kineticball.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
 var initial_direction = rand_range(0,359)
-
+export (float,-1,1,0.1) var Bounce_Coeff = 1
+export (float,-1,1,0.1) var Friction_Coeff = 1
+export (float,-1,1,0.1) var Linear_Coeff = 1
 
 func _process(delta):
-	bounce = 1
-	friction = 0
-	linear_damp = 0
+	bounce = Bounce_Coeff
+	friction = Friction_Coeff
+	linear_damp = Linear_Coeff
 	scale = Vector2(0.5,0.5)
 	$CollisionShape2D.scale = Vector2(0.5,0.5)
 	var current_speed = linear_velocity
